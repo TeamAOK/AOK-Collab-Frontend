b14d9284268ff258c25824a363a533185ea71b0a
sahilarora3117 <iamsahil@protonmail.com>
2021-08-15 09:54:01 +0530

    added basic routes with protection

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,57 @@
-import React from 'react';
-import './App.css';
-import SignUp from './Components/SignUp/SignUp'
-function App() {
+import { lazy, Suspense } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+// import Loader from 'shareComponent/Loader';
+import ProtectedRoutes from './Routes/ProtectedRoutes'; //Authenticated routes
+import PublicRoute from './Routes/PublicRoutes'; 
+import PrivateRoute from './Routes/PrivateRoutes'; 
+
+const LoginPage = lazy(() => import('./Components/Authenticate/SignUp'));
+const Register = lazy(() => import('./Components/Authenticate/Login'));
+const NoFoundComponent = lazy(() => import('./Components/404NoComponent/NoComponent'));
+
+const App = () => {
+  const isAuthenticated = true;
+
   return (
-    <div className="container mx-auto">
-      <SignUp />
-    </div>
+    <Router>
+      <Suspense fallback={<div>Loading</div>}>
+        <Switch>
+        <PublicRoute
+            path="/register"
+            isAuthenticated={isAuthenticated}
+          >
+            <Register />
+          </PublicRoute>
+          <PublicRoute
+            path="/login"
+            isAuthenticated={isAuthenticated}
+          >
+            <LoginPage />
+          </PublicRoute>
+          
+          <PublicRoute
+            path="/forgot-password"
+            isAuthenticated={isAuthenticated}
+          >
+          </PublicRoute>
+          <PrivateRoute
+            path="/"
+            isAuthenticated={isAuthenticated}
+          >
+            <ProtectedRoutes />
+          </PrivateRoute>
+          <Route path="*">
+            <NoFoundComponent />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/Components/Authenticate/SignUp.tsx
+++ src/Components/Authenticate/SignUp.tsx
@@ -51,7 +51,7 @@ export default function SignUp() {
                     <p className="text-red-500 text-xs italic">Please choose a password.</p>
                 </div>
                 <div className="flex items-center justify-between">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={() => signUpCallback()} type="button">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={() => console.log("Nopything")} type="button">
                         Sign Up
                     </button>
                 </div>
